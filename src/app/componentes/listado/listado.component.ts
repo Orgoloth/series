615c048594aa0375ccc88ab0407ddc7c0028d23c
Manuel <orgoloth@hotmail.com>
2020-08-12 00:17:13 +0200

    1

--- src/app/componentes/listado/listado.component.ts
+++ src/app/componentes/listado/listado.component.ts
@@ -11,20 +11,16 @@ export class ListadoComponent implements OnInit {
   pagina: number = 1;
   totalPaginas: number = 2;
   contador: number = 0;
+
   myObserver = {
     next: (data) => {
+      console.log(data);
       this.results = [...this.results, ...data['results']];
       this.pagina = data['page'];
       this.totalPaginas = data['total_pages'];
     },
     error: (err) => console.error('Observer got an error: ' + err),
-    complete: () => {
-      console.log(
-        `Traida página ${this.pagina} de ${this.totalPaginas}`,
-        this.results
-      );
-      this.ordenarResultados();
-    },
+    complete: () => this.ordenarResultados(),
   };
 
   constructor(private tmdb: TmdbService) {}
@@ -36,11 +32,10 @@ export class ListadoComponent implements OnInit {
   traerResultados() {
     do {
       this.tmdb.searchTv('dark', ++this.contador).subscribe(this.myObserver);
-    } while (this.contador < 3 && this.pagina < this.totalPaginas);
+    } while (this.contador < 10 && this.contador <= this.totalPaginas);
   }
 
   ordenarResultados() {
     this.results.sort((a, b) => (a.popularity > b.popularity ? -1 : 1));
-    console.log('Ordenando resultados');
   }
 }
